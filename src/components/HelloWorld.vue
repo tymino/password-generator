179544eac9270e593740d7a93824e8f6f7a32f39
Aleksey <d_team-lex@bk.ru>
2023-05-07 15:46:22 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h3>Installed CLI Plugins</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+});
+</script>
+
+<style scoped lang="scss">
+</style>
